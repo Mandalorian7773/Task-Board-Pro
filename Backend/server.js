d3ffaaca2b35dd2c81a14998f2d8669ed9b436a5
Mandalorian7773 <adityajagrani8@gmail.com>
2025-05-17 14:25:25 +0530

    fixed middleware error

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
-const { verifyToken } = require('./middleware/auth.middleware');
+const { verifyToken } = require('./Middleware/auth.middleware');
 
 
 require('dotenv').config();
